ec610a12ec3e8c901a23322f5742fb43a36bb77c
Ellen <5754439+lirenn@users.noreply.github.com>
2019-04-09 16:05:24 +1200

    Rearranges files into correct folder structure.
    - Added autogenerated Gradle files instead of using ANT.

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,9 +1,5 @@
 import static org.junit.Assert.*;
-import org.bson.Document;
 import org.junit.Test;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 public class DollarTest {
 	@Test
